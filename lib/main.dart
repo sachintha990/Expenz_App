a4c6d8ddf595d2ef2702b0d2cdc7e9f7cfc94a5e
sachintha990 <sachintharathnayaka12@gmail.com>
2025-04-17 12:41:53 +0530

    Implement onboarding feature with multiple screens and navigation; add onboarding data model and UI components

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:expez_app/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -10,20 +11,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'expez_app',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        fontFamily: "Inter",
-      ),
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        body: Center(
-          child: Text(
-            "Hello Flutter",
-            style: TextStyle(fontSize: 30),
-          ),
+        title: 'expez_app',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          fontFamily: "Inter",
         ),
-      ),
-    );
+        debugShowCheckedModeBanner: false,
+        home: OnBoardingScreen());
   }
 }
---
+++ lib/screens/onboarding_screen.dart
@@ -0,0 +1,117 @@
+import 'package:expez_app/data/onboarding_data.dart';
+import 'package:expez_app/screens/onboarding/front_page.dart';
+import 'package:expez_app/screens/onboarding/shared_screen.dart';
+import 'package:expez_app/screens/user_data_screen.dart';
+import 'package:expez_app/utils/colors.dart';
+import 'package:expez_app/widgets/custum_button.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+
+class OnBoardingScreen extends StatefulWidget {
+  const OnBoardingScreen({super.key});
+
+  @override
+  State<OnBoardingScreen> createState() => _OnBoardingScreenState();
+}
+
+class _OnBoardingScreenState extends State<OnBoardingScreen> {
+  final PageController _controller = PageController();
+
+  bool showHomePage = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                //Onboarding pages list is passed here
+                PageView(
+                  controller: _controller,
+                  onPageChanged: (index) {
+                    setState(() {
+                      showHomePage = index == 3;
+                    });
+                  },
+                  children: [
+                    const FrontPage(),
+                    SharedOnboardingScreen(
+                      title: OnbardingData.onboardingList[0].title,
+                      imagePath: OnbardingData.onboardingList[0].imagePath,
+                      description: OnbardingData.onboardingList[0].description,
+                    ),
+                    SharedOnboardingScreen(
+                      title: OnbardingData.onboardingList[1].title,
+                      imagePath: OnbardingData.onboardingList[1].imagePath,
+                      description: OnbardingData.onboardingList[1].description,
+                    ),
+                    SharedOnboardingScreen(
+                      title: OnbardingData.onboardingList[2].title,
+                      imagePath: OnbardingData.onboardingList[2].imagePath,
+                      description: OnbardingData.onboardingList[2].description,
+                    ),
+                  ],
+                ),
+
+                //the page indicators
+
+                Container(
+                  alignment: const Alignment(0, 0.75),
+                  child: SmoothPageIndicator(
+                    controller: _controller,
+                    count: 4,
+                    effect: const WormEffect(
+                      activeDotColor: kMainColor,
+                      dotColor: kLightGrey,
+                    ),
+                  ),
+                ),
+
+                //navigation button
+
+                Positioned(
+                  bottom: 20,
+                  left: 0,
+                  right: 0,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: !showHomePage
+                        ? GestureDetector(
+                            onTap: () {
+                              _controller.animateToPage(
+                                _controller.page!.toInt() + 1,
+                                duration: const Duration(milliseconds: 400),
+                                curve: Curves.easeInOut,
+                              );
+                            },
+                            child: CustumButton(
+                              buttonColor: kMainColor,
+                              buttonName: showHomePage ? "Get Started" : "Next",
+                            ),
+                          )
+                        : GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const UserDataScreen(),
+                                ),
+                              );
+                            },
+                            child: CustumButton(
+                              buttonColor: kMainColor,
+                              buttonName: showHomePage ? "Get Started" : "Next",
+                            ),
+                          ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
